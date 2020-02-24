0fe41432be95664355509d98694305cf9c6603d9
Fênix <59613474+FFenixx@users.noreply.github.com>
2020-02-24 17:11:11 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-    const ms = await message.channel.send("Pong!");
-	const clientms = ms.createdTimestamp - message.createdTimestamp;
+    const msg = await message.channel.send("Pong!");
+	const clientms = msg.createdTimestamp - message.createdTimestamp;
 	ms.edit(`Ping: ${clientms} | WebSocket: ${Math.round(client.ping)}`); 
 }
