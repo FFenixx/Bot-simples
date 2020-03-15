dadde1c977ed607408b5ea80bbc3926b0f284f40
Fênix <59613474+FFenixx@users.noreply.github.com>
2020-03-15 13:43:08 -0300

    Update limpar.js

--- commands/limpar.js
+++ commands/limpar.js
@@ -1,12 +1,17 @@
 exports.run = async (client, message, args) => {
 
     message.delete()
-    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(`${message.author}, Você deve ter a permissão de gerenciar mensagens para usar o comando!`)
-        const deleteCount = parseInt(args[0], 10);
-        if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-            return message.reply("<:x_:657038227349176320>   Diga de 2 à 100 mensagens para excluir.");
-        const fetched = await message.channel.fetchMessages({ limit: deleteCount });
-        message.channel.bulkDelete(fetched)
-            .catch(error => message.channel.send("Ocorreu um erro."));
-                        message.channel.send(`${deleteCount} mensagens deletadas deletadas.`)
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`permissao insulficiente.`)
+    message.delete()
+    const amount = parseInt(args[0]);
+    if (isNaN(amount)) {
+	return message.reply('informe uma quantidade para apagar.');
+    } else if (amount < 2 || amount > 100) {
+        
+	return message.reply('informe uma quantidade de 2 a 99 mensagens.');
+}
+        message.channel.bulkDelete(amount, true).catch(err => {
+	console.error(err);
+	message.channel.send(`${mount} mensagens foram apagadas nesse canal.`);
+});
 }
