e769f2899d69061c8e5333c1f7da9b38cde74af1
Fênix <59613474+FFenixx@users.noreply.github.com>
2020-03-15 13:34:10 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,12 +1,11 @@
 exports.run = (client, message, args) => {
 
-if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("você não tem permissão para kickar ninguém."); // permissão que o membro precisa.
-
- if(args.length === 0) return message.reply("Mencione um mebro para banir.");
+if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("você não tem permissão para banir ninguém."); // permissão que o membro precisa.
  
  let membro = message.mentions.users.first(); // mencionar o usuário 
+ if(!membro) return message.reply("mencione um membro para kickar.")
  
- message.guild.member(membro).kick() // kicka o membro,
+message.guild.members.kick(membro.id) // kicka o membro,
  message.channel.send(`O membro ${membro} foi kickado com sucesso!`)
  
  }
