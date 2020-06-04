359b72c8a43574bdda46663ebc9e88c7feb7ccb6
Fênix <59613474+FFenixx@users.noreply.github.com>
2020-06-04 12:50:53 -0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = async (client, member) => {
 
-let canal = member.guild.channels.find(canal => canal.name === "geral"); // nome do canal
+let canal = member.guild.channels.cache.find(canal => canal.name === "geral"); // nome do canal
 canal.send(`${member} entrou.`)
 }
